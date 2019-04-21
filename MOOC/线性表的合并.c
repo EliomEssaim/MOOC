d019af0b1e565aeb2026dd3482bdc3a89ed0e0fd
dragon jack <31036659+EliomEssaim@users.noreply.github.com>
2019-04-22 07:57:34 +0800

    为什么L2会被修改呢？

--- MOOC/线性表的合并.c
+++ MOOC/线性表的合并.c
@@ -18,19 +18,11 @@ int main()
 {
 	List L1, L2, L;
 	L1 = Read();
-	L = (List)malloc(sizeof(struct Node));
-	L = L1;
-	while (L)
-	{
-		L = L->Next;
-		printf("%d",L->Data);
-		
-	}
-	//	L2 = Read();
-	//    L = Merge(L1, L2);
-	//    Print(L);
-	//    Print(L1);
-	//    Print(L2);
+	L2 = Read();
+	L= Merge(L1, L2);
+	Print(L);
+	Print(L1);
+	Print(L2);
 	return 0;
 }
 List Read() {
@@ -49,7 +41,72 @@ List Read() {
 		last = p_new;
 		n--;
 	}
+	
 	return p;
+}
+List Merge(List L1, List L2)
+{
+	List L = (List)malloc(sizeof(struct Node));
+	List last = (List)malloc(sizeof(struct Node));
+	L->Next = NULL;
+	last = L;
+	L1 = L1->Next;
+	L2 = L2->Next;
+	while ((L1 != NULL) && (L2 != NULL))
+	{
+		if (L1->Data < L2->Data)
+		{
+			last->Next = L1;
+			last = last->Next;
+			L1 = L1->Next;
+		}
+		else
+		{
+			last->Next = L2;
+			last = last->Next;
+			L2 = L2->Next;
+		}		
+	}
+	if (L1 == NULL)
+	{
+		while (L2)
+		{
+			last->Next = L2;
+			last = last->Next;
+			L2 = L2->Next;
+		}
+	}
+	else if (L2 == NULL)
+	{
+		while (L1)
+		{
+			last->Next = L1;
+			last = last->Next;
+			L1 = L1->Next;
+		}
+	}
+	last->Next = NULL;
+	
+	return L;
+}
+void Print(List L)
+{
+	if (L->Next == NULL)
+	{
+		printf("NULL\n");
+		return;
+	}
+	else
+	{
+		L = L->Next;
+		while (L)
+		{
+
+			printf("%d ", L->Data);
+			L = L->Next;
+		}
+	}
+	printf("\n");
 }
 //小知识：结构的大小是其成员大小的最小公倍数（但要大于和）本质是结构体地址需要对齐 
 //小知识;scanf("%d",&a)会忽略空格
