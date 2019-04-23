dffc622cdb4c8f5e938e4d6f78ae285658b5ae41
dragon jack <31036659+EliomEssaim@users.noreply.github.com>
2019-04-23 21:57:03 +0800

    完成等测试

--- MOOC/ReversLinkedList.cpp
+++ MOOC/ReversLinkedList.cpp
@@ -13,22 +13,26 @@ int main()
 {
 	int RevScale, num,First,add,tmp,i=0;
 	int List_add[MAXSIZE];
-	scanf("%d %d % d", &First, &num, &RevScale);
+	scanf("%d %d %d", &First, &num, &RevScale);
 
 	for (int i = 0; i < num; i++)
 	{
-		scanf("%d %d %d", &add, &node[add].Data, &node[add].Next);
+		scanf("%d", &add); 
+		scanf("%d%d",&node[add].Data, &node[add].Next);
 	}
-
+	i = 0;
 	tmp = First;
 	while (node[tmp].Next != -1)
 	{
 		List_add[i++] = tmp;
 		tmp = node[tmp].Next;
 	}
-	for(int i=num%RevScale;i;i--)
-
-
+	for (i = num % RevScale; i; i--)
+	{
+		reverse(List_add+(i-1)*RevScale,List_add+(i-1)* RevScale);
+	}
+	for (int i = 0; i < num; i++)
+		printf("%d %d %d", List_add[i], node[List_add[i]].Data, List_add[i + 1]);
 	system("pause");
 	return 0;
 }
