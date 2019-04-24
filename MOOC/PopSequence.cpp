f506e6004b65010f39db5d70a999e24aa2350dfd
dragon jack <31036659+EliomEssaim@users.noreply.github.com>
2019-04-24 22:45:25 +0800

    独立完成了一个虽然简单确是独立完成的算法

---
+++ MOOC/PopSequence.cpp
@@ -0,0 +1,81 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+
+#define MAXSIZE 1000
+bool Rule1(int *List);
+bool Rule2(int *List);
+int StackCapacity, NumPush, NumPop;
+int main()
+{
+	
+	int *input;
+	bool result[MAXSIZE];
+	scanf("%d%d%d", &StackCapacity, &NumPush, &NumPop);
+	input = (int*)malloc(NumPush * sizeof(int));
+	for (int i = 0; i < NumPop; i++)
+	{
+		for (int j = 0; j < NumPush; j++)
+			scanf("%d",&input[j]);
+		result[i] = Rule1(input)&Rule2(input);
+		 
+	}
+
+	for (int i = 0; i < NumPop; i++)
+	{
+		if (result[i])
+			printf("YES\n");
+		else
+			printf("NO\n");
+	}
+
+	system("pause");
+	return 0;
+}
+bool Rule1(int *List)
+{
+	for (int i = 0; i < NumPush - StackCapacity; i++)
+	{
+		if (List[i] > (StackCapacity + i))
+			return 0;
+		
+	}
+	return 1;
+}
+bool Rule2(int *List)
+{
+	bool flag=1;
+	int LogicLast;
+	
+	
+	for (int i = 0; i < NumPush - 1; i++)
+	{
+		LogicLast = List[i] - 1;
+		for (int j = i-1; j >= 0; j--)
+		{
+			if (List[j] == LogicLast)
+			{
+				LogicLast--;
+				j = i;
+			}
+		}
+
+		
+		
+		if (List[i] > List[i+1] && List[i+1] != LogicLast)
+			return 0;
+			
+	}
+	return 1;
+}
+//耗时长：
+//原因：思路一开始没想好
+//我的思路：
+//设置规则两条：1输入的数不超过数组的容纳量 2 出栈顺序正确
+//1 2耗时都比较长尤其是2 最开始没有搞清楚角色要怎么实现
+//第一次想 搞个inside记录下一个可能的值（后面的数如果小于前面的，那么后面的数是可以唯一确定的）
+//实现是先给inside按顺序赋值然后如果遇到前面的数就取消 后来无法实现或者复杂度高
+//最后才意识到根本没有必要 只要确定一个唯一值就好了
+
+//dalao的思路;
--- MOOC/ReversLinkedList.cpp
+++ MOOC/ReversLinkedList.cpp
@@ -9,10 +9,12 @@ struct Node {
 	int Data;
 	int Next;
 }node[MAXSIZE];
+int List_add[MAXSIZE];//写在main里面会导致堆栈异常
 int main()
 {
 	int RevScale, num,First,add,tmp,i=0;
-	int List_add[MAXSIZE];
+	int RevCnt;
+
 	scanf("%d %d %d", &First, &num, &RevScale);
 
 	for (int i = 0; i < num; i++)
@@ -22,17 +24,25 @@ int main()
 	}
 	i = 0;
 	tmp = First;
-	while (node[tmp].Next != -1)
+	while (tmp != -1)//这里写错成node[tmp].Next这样会导致最后一个元素无法录入
 	{
 		List_add[i++] = tmp;
 		tmp = node[tmp].Next;
 	}
-	for (i = num % RevScale; i; i--)
+	
+	num = i;
+	for (i = num / RevScale; i&&(RevScale <= num); i--)
 	{
-		reverse(List_add+(i-1)*RevScale,List_add+(i-1)* RevScale);
+		reverse(&List_add[(i-1)*RevScale],&List_add[i * RevScale]);
 	}
-	for (int i = 0; i < num; i++)
-		printf("%d %d %d", List_add[i], node[List_add[i]].Data, List_add[i + 1]);
-	system("pause");
+	RevCnt = num % RevScale;
+	for (i = 0; i < num-1; i++)
+		printf("%05d %d %05d\n", List_add[i], node[List_add[i]].Data, List_add[i + 1]);
+	printf("%05d %d %d\n", List_add[i], node[List_add[i]].Data, -1);
+
 	return 0;
 }
+//有多余结点不在链表上 
+//意思是，我输入N个结点，但不是说这N个结点都在这个链表上。
+//其中几个有可能是游离的（地址并不相连）。
+//所以一开始的count和后面的链表的元素个数并不一定相等。
