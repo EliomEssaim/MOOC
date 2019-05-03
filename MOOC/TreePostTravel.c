9beaf0960c45cdb0d4df154d3dc3daf7f0f830fc
coder-big <863040538@qq.com>
2019-05-03 11:05:27 +0800

    后序遍历的实现

---
+++ MOOC/TreePostTravel.c
@@ -0,0 +1,14 @@
+//需要掌握的知识：
+//1、如何建立一个堆栈？以及对应的操作集 创建 push pop 判断满 判断空
+//2、后序遍历的递归实现
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdbool.h>
+#include "malloc.h"
+#include "string.h"
+ 
+int main()
+{
+	return 0;
+}
+
